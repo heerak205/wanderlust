05af035e8e6a3bf8f45fb65c981b012abab38617
heerak205 <heerak.praffull@msitprogram.net>
2018-08-04 10:21:53 +0530

    " -> 4 of 7 passed. pylint: 10/10 "

--- cspp1 assignments/m6/p1/fizz_buzz.py
+++ cspp1 assignments/m6/p1/fizz_buzz.py
@@ -12,11 +12,12 @@ def main():
     i = 0
     while i < num:
         i = i+1
-        if i%3 == 0:
+        if i%3 == 0 and i%5 == 0:
+            print("FizzBuzz")
+        elif i%3 == 0:
             print("Fizz")
         elif i%5 == 0:
             print("Buzz")
         else:
             print(i)
-
 main()
