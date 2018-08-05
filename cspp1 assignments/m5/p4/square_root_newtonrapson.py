562a3da6a6f2e66436ea7013e545d4254cf2572a
heerak205 <heerak.praffull@msitprogram.net>
2018-08-05 19:17:50 +0530

    " -> 3 of 3 passed. pylint: 0/10 "

---
+++ cspp1 assignments/m5/p4/square_root_newtonrapson.py
@@ -0,0 +1,20 @@
+# Write a python program to find the square root of the given number
+# using approximation method
+
+# testcase 1
+# input: 25
+# output: 4.999999999999998
+
+# testcase 2
+# input: 49
+# output: 6.999999999999991
+
+
+
+epsilon = 0.01
+x_num = int(input())
+guess_val = x_num/2.0
+while abs(guess_val*guess_val - x_num) >= epsilon:
+    guess_val = guess_val - (((guess_val**2) - x_num)/(2*guess_val))
+print(str(guess_val))
+
