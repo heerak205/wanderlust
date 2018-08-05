aefad54509d58c8a58a6419a17a69752773446c3
heerak205 <heerak.praffull@msitprogram.net>
2018-08-05 19:15:13 +0530

    " -> 3 of 3 passed. pylint: 9/10 "

--- cspp1 assignments/m5/p3/square_root_bisection.py
+++ cspp1 assignments/m5/p3/square_root_bisection.py
@@ -9,16 +9,15 @@
 # input: 49
 # output: 6.999999999999991
 
-def main():
-    x_num = int(input())
-    epsilon = 0.01
-    lower = 0.0
-    higher = x_num
-    bi_val = (higher + lower)/2.0
-    while abs(bi_val**2 - x_num) > epsilon:
-        if bi_val**2 < x_num:
-            lower = bi_val
-        higher = bi_val
-    bi_val = (higher + lower)/2.0
-print(bi_val)
-main()
+X_NUM = int(input())
+EPSILON = 0.01
+LOWER = 0.0
+HIGHER = X_NUM
+BI_VAL = (HIGHER + LOWER)/2.0
+while abs(BI_VAL**2 - X_NUM) > EPSILON:
+    if BI_VAL**2 < X_NUM:
+        LOWER = BI_VAL
+    else:
+        HIGHER = BI_VAL
+    BI_VAL = (HIGHER + LOWER)/2.0
+print(BI_VAL)
