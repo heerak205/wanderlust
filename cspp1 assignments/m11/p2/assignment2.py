d6128a461b791beaecb82c9313ff8cfda3a1edfb
heerak205 <heerak.praffull@msitprogram.net>
2018-08-20 15:47:31 +0530

    " -> 6 of 6 passed. pylint: 7/10 "

--- cspp1 assignments/m11/p2/assignment2.py
+++ cspp1 assignments/m11/p2/assignment2.py
@@ -1,12 +1,16 @@
-"""#Exercise: Assignment-2
-#Implement the updateHand function
-"""
-def updateHand(hand, word):
+"""program to update hand"""
+#Exercise: Assignment-2
+#Implement the updateHand function.
+#Make sure this function has no side effects: i.e.,
+#it must not mutate the hand passed in. Before pasting
+#your function definition here, be sure you've passed the
+#appropriate tests in test_ps4a.py.
+def update_hand(hand, word):
     """
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
     a letter appears in 'word', 'hand' has at least as
-    many of that letter in it. 
+    many of that letter in it.
     Updates the hand: uses up the letters in the given word
     and returns the new hand, without those letters in it.
     Has no side effects: does not modify hand.
@@ -14,22 +18,21 @@ def updateHand(hand, word):
     hand: dictionary (string -> int)
     returns: dictionary (string -> int)
     """
-    a_dict = {}
-    for var_j in word:
-        if var_j in hand:
-            a_dict[var_j] = hand[var_j]-1
-    return a_dict
+    for letter in word:
+        if letter in hand:
+            hand[letter] = hand[letter] - 1
+    return hand
+"""
+program to take the input
+"""
 def main():
-    """
-    main function called.
-    """
-    input_n = input()
-    a_dict = {}
-    for _ in range(int(input_n)):
-        var_data = input()
-        l_l = var_data.split()
-        a_dict[l_l[0]] = int(l_l[1])
-    var_data1 = input()
-    print(updateHand(a_dict, var_data1))
+    n_n = input()
+    adict = {}
+    for i in range(int(n_n)):
+        data = input()
+        l = data.split()
+        adict[l[0]] = int(l[1])
+    data1 = input()
+    print(update_hand(adict, data1))
 if __name__ == "__main__":
     main()
