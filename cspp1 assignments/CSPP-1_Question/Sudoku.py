bd3d069f479a5ed92c0d4853037d2ce132583849
heerak205 <heerak.praffull@msitprogram.net>
2018-12-25 19:52:20 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -3/10 "

--- cspp1 assignments/CSPP-1_Question/Sudoku.py
+++ cspp1 assignments/CSPP-1_Question/Sudoku.py
@@ -1,64 +1,143 @@
-def check(sudoku):
-    if (sudoku.count('.') == 0):
-        return 'Given sudoku is solved'
+"""
+In this method :
+ * Check there are only 81 values
+ * iterate through each row in the sudoku and if you find any duplicate values
+ 	raise an exception
+ * iterate through each column in the sudoku and if you find any duplicate values
+	raise an exception
+ * iterate through each subgrid(3x3) in the sudoku and if you find any duplicate values
+	raise an exception
+"""
+def validateSudoku(sudoku):
+	if len(sudoku) != 81:
+		raise Exception("Invalid input")
+	p = getRowValues(sudoku)
+	for i in range(9):
+		if len(set(p[i])) != 9:
+			raise Exception("Invalid input")
+	q = getColumnValues(p)
+	for i in range(9):
+		if len(set(q[i])) != 9:
+			raise Exception("Invalid input")
+"""
+This  method should retunn all the values present in the ith row
+"""
+def getRowValues(r):
+	# n = 9
+	# #print("HI")
+	# for i in range(0, len(r), n):
+	# 	yield r[i:i+n]
+	# return r
+	list3 = []
+	list4 = []
+	for i in range(82):
+		if(i == 81):
+			list4.append(list3)
+			break
+		if(i%9 == 0 and i != 0):
+			list4.append(list3)
+			list3 = []
+		list3.append(r[i])
+	#print(list4)
+	return list4
 
-    elif(len(sudoku) != 81):
-        return 'Invalid input'
+"""
+This  method should retunn all the values present in the ith column
+"""
+def getColumnValues(q):
+	list1 = []
+	list2 = []
+	for i in range (9):
+		for j in range(9):
+			list1.append(q[j][i])
+		list2.append(list1)
+		list1 = []
+	#print(list2)
+	return list2
 
-    else:
-        collection_rows = []
-        for i in range(0,len(sudoku),9):
-            row_ = [int(j) for j in sudoku[i:i+9] if j != '.']
-            set_row = {k for k in row_}
-            if len(row_) != len(set_row):
-                return 'Invalid Sudoku:Duplicate values'
-            elif collection_rows.count(row_) == 1:
-                return 'Invalid Sudoku:Duplicate values'
-            else:
-                collection_rows.append(row_)
-        return ''
+"""
+This  method should retunn all the values present in the i,j th subgrid
+"""
+def getGridValues(s):
+	#c = getRowValues(s)
+	#d = getColumnValues(c)
+	list5 = []
+	list6 = []
+	for i in range(3):
+		for j in range(3):
+			list5.append(s[i][j])
+	list6.append(list5)
+	list5 = []
+	for i in range(3):
+		for j in range(3,6):
+			list5.append(s[i][j])
+	list6.append(list5)
+	list5 = []
+	for i in range(3):
+		for j in range(6,9):
+			list5.append(s[i][j])
+	list6.append(list5)
+	#print(list6)
+	list5 = []
+	for i in range(3,6):
+		for j in range (3):
+			list5.append(s[i][j])
+	list6.append(list5)
+	list5 = []
+	for i in range(3,6):
+		for j in range(3,6):
+			list5.append(s[i][j])
+	list6.append(list5)
+	list5 = []
+	for i in range(3,6):
+		for j in range(6,9):
+			list5.append(s[i][j])
+	list6.append(list5)
+	list5 = []
+	for i in range(6,9):
+		for j in range(3):
+			list5.append(s[i][j])
+	list6.append(list5)
+	list5 = []
+	for i in range(6,9):
+		for j in range(3,6):
+			list5.append(s[i][j])
+	list6.append(list5)
+	list5 = []
+	for i in range(6,9):
+		for j in range(6,9):
+			list5.append(s[i][j])
+	list6.append(list5)
+	return list6
+	#print(list6)
 
-def possiblities(sudoku):
-    check= [False]*9
-    count = 0
-    i = ''
-    for i in sudoku:
-        if count == 9:
-            # print(check)
-            for j in range(9):
-                if check[j] == False:
-                    print(j+1)
-            check = [False]*9
-            # print(check)
-            count = 0
-        #   count = count + 1
-        #   if(i == '.'):
-        #       continue
-        #   check[int(i) - 1] = True
-        # else:
-        count = count + 1
-        if(i == '.'):
-            continue
-        check[int(i) - 1] = True
-    if count == 9:
-            # print(check)
-            for j in range(9):
-                if check[j] == False:
-                    print(j+1)
-            check = [False]*9
-            # print(check)
-            count = 0
+
+		
+
+"""
+This method should collect all the available values present for a "."
+You should get the values present in row,column,grid.
+Then you should return the values that doesnot exist in the previous values.
+"""
+def possibleValues():
+	pass
+	
+"""
+Read the input and store the values in an appropriate data sturcture.
+Then travese through each value, if you get a "." then collect the possible values
+
+"""
 
 
 def main():
-    sudoku_ = input()
-    check_result = check(sudoku_)
-    if check_result != '':
-        print(check_result)
-        return
-    possiblities(sudoku_)
-        
-
+	q = list(input())
+	r = getRowValues(q)
+	getColumnValues(r)
+	try:
+		validateSudoku(q)
+	except Exception as e:
+		print(e)
+	getGridValues(r)
 
 if __name__ == "__main__":
     main()
