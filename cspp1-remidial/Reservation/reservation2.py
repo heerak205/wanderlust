9f154d66dcc71877667debdf697f097e64052fb0
heerak205 <heerak.praffull@msitprogram.net>
2019-01-04 00:59:56 +0530

    "cspp2_m10_a1_1: 5/8 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 0/0 "

---
+++ cspp1-remidial/Reservation/reservation2.py
@@ -0,0 +1,53 @@
+def main():
+	n = int(input())
+	i = 0
+	hotdict = {}
+	roomno = 1
+	limit = 5
+	while i<n:
+		# print(i)
+		b = input().split(" ")
+		if len(b) == 2:
+			if b[0] == 'reserve':
+				if roomno > limit:
+					print("All Rooms are reserved")
+				if roomno <= limit+1:
+					for m in range(1, limit+1):
+						if m not in hotdict.keys():
+							hotdict.update({m:b[1]})
+							print(hotdict[m] + " " + str(m))
+							roomno = roomno + 1
+							break
+				else:
+					if roomno not in hotdict.keys():
+						hotdict.update({roomno:b[1]})
+						print(hotdict[roomno] + " " + str(roomno))
+						roomno = roomno + 1
+			if b[0] == "build":
+				limit = limit + int(b[1])
+				print("Added" + " " + b[1] + " " + "more rooms")
+			if b[0] == "cancel":
+				print(b[1] + " now has no reservations")
+				for each in hotdict.keys():
+					if hotdict.get(each) == b[1]:
+						del hotdict[each]
+						break
+
+				# print("Room is already reserved")
+		if len(b) == 3:
+			if roomno > limit:
+				print("All Rooms are reserved")
+			elif int(b[2]) not in hotdict.keys():
+				hotdict.update({int(b[2]):b[1]})
+				print(hotdict[int(b[2])] + " " + str(b[2]))
+				roomno = roomno + 1
+			else:
+				print("Room is already reserved")
+		if len(b) == 1:
+			p = sorted(hotdict)
+			for each in p:
+				print(hotdict[each] + " " + str(each))
+		i = i + 1
+
+if __name__ == '__main__':
+	main()
