c21506dae6a1390d0c54cc672c4fae833f3bdb04
heerak205 <heerak.praffull@msitprogram.net>
2019-01-05 11:35:15 +0530

    "cspp1_m10_a1_1: 3/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -6/10 "

--- cspp1-remidial/CSPP1Week-2Exam testcases/list3.py
+++ cspp1-remidial/CSPP1Week-2Exam testcases/list3.py
@@ -3,34 +3,36 @@ def main():
 	list1 = eval(list2)
 	i = 0
 	sum = 0
-	a = 0
 	res = 0
-	while i<len(list1):
-		if type(list1[i]) == list:
-			j = 0
-			while j<len(list1[i]):
-				if type(list1[i][j]) == list:
-					k = 0
-					while k<len(list1[i][j]):
-						if type(list1[i][j][k]) == list:
-							l = 0
-							while l<len(list1[i][j][k]):
+	if len(list1) == 1:
+		print(0)
+	else:
+		while i<len(list1):
+			if type(list1[i]) == list:
+				j = 0
+				while j<len(list1[i]):
+					if type(list1[i][j]) == list:
+						k = 0
+						while k<len(list1[i][j]):
+							if type(list1[i][j][k]) == list:
+								l = 0
+								while l<len(list1[i][j][k]):
+									if type(list1[i][j][k]) != str:
+										sum = sum + list1[i][j][k][l]
+									l = l + 1
+							else:
 								if type(list1[i][j][k]) != str:
-									sum = sum + list1[i][j][k][l]
-								l = l + 1
-						else:
-							if type(list1[i][j][k]) != str:
-								sum = sum + list1[i][j][k]
-						k = k + 1
-				else:
-					if type(list1[i][j]) != str:
-						sum = sum + list1[i][j]
-				j = j+1
-		else:
-			if type(list1[i]) != str:
-				sum = sum + list1[i]
-		i = i + 1
-	print(sum)
+									sum = sum + list1[i][j][k]
+							k = k + 1
+					else:
+						if type(list1[i][j]) != str:
+							sum = sum + list1[i][j]
+					j = j+1
+			else:
+				if type(list1[i]) != str:
+					sum = sum + list1[i]
+			i = i + 1
+		print(sum)
 	for each in list2:
 		if each == '[' or each == ']':
 			res = res + 1
