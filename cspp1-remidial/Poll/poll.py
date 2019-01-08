51165d0356daaf8ab0e8e24aee54723fb99e2a00
heerak205 <heerak.praffull@msitprogram.net>
2019-01-08 20:59:34 +0530

    "cspp2_m10_a1_1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -4/10 "

--- cspp1-remidial/Poll/poll.py
+++ cspp1-remidial/Poll/poll.py
@@ -1,39 +1,85 @@
+from collections import Counter
 def main():
 	a = int(input())
 	b = a*5
-	i = 0
-	global votedict
-	votedict = {}
-	global queslist
 	queslist = []
-	while i<b:
-		inp = input()
-		queslist.append(inp)
-		i = i + 1
-
-	voters = int(input())
-	c = voters*(a+1)
-	j = 0
-	global voterlist
-	voterlist = []
-	while j<c:
-		d = input().split(" ")
-		voterlist.append(d)
-		j = j + 1
+	quesdict = {}
+	for i in range(0, b):
+		c = input()
+		queslist.append(c)
 	# print(queslist)
-	# print(voterlist)
-	print(voterdict(voterlist))
-
-def voterdict(lis):
-	global votedict
-	for each in lis:
-		if len(each) == 2:
-			if each[1] not in votedict:
-				votedict[each[1]] = int(each[0])
+	newqueslist = []
+	i = 0
+	while i<len(queslist):
+		newlist1 = []
+		for j in range(i, i+5):
+			newlist1.append(queslist[j])
+		newqueslist.append(newlist1)
+		i = i + 5
+	# print(newqueslist)
+	queslist1 = newqueslist
+	i = 0
+	d = 1
+	while i<len(newqueslist):
+		quesdict[d] = newqueslist[i]
+		d = d + 1
+		i = i + 1
+	# print(quesdict)
+	adict = {}
+	noofpart = int(input())
+	# print(noofpart)
+	totpart = noofpart*(a+1)
+	# print(totpart)
+	i = 0
+	partlist = []
+	while i<totpart:
+		inp = input()
+		partlist.append(inp)
+		i = i + 1
+	# print(partlist)
+	major = Counter(partlist)
+	# print(major)
+	adict1 = {}
+	adict2 = {}
+	adict3 = {}
+	adict4 = {}
+	for key, value in major.items():
+		p = key.split(" ")
+		# print(p)
+		if len(p) == 2:
+			if int(p[0]) == 1 :
+				adict1[p[1]] = value
+			elif int(p[0]) == 2 :
+				adict2[p[1]] = value
+			elif int(p[0]) == 3:
+				adict3[p[1]] = value
 			else:
-				votedict[each[1]] = votedict.get(each[1]) + int(each[0])
-	return votedict
+				adict4[p[1]] = value
+
+	# print(adict1)
+	# print(adict2)
+	# print(adict3)
+	# print(adict4)
+	list1 = []
+	for key, value in adict1.items():
+		list1.append(value)
+	for each in adict1:
+		if adict1[each] == max(list1):
+			print("Highest number of votes for question : Who should be the next Prime Minister? : "+each)
+	list2 = []
+	if len(adict2) != 0:
+		for key, value in adict2.items():
+			list2.append(value)
+		for each in adict2:
+			if adict2[each] == max(list2):
+				print("Highest number of votes for question : Who will be the next cm for Telangana? : "+each)
+	list3 = []
+	if len(adict3) != 0:
+		for key, value in adict3.items():
+			list3.append(value)
+		for each in adict3:
+			if adict3[each] == max(list3):
+				print("Highest number of votes for question : Who will be the next cm for AP? : "+each)
 
 if __name__ == '__main__':
 	main()
-
--- cspp1-remidial/Poll/poll1.py
+++ cspp1-remidial/Poll/poll1.py
@@ -1,4 +1,3 @@
-from collections import Counter
 def main():
 	a = int(input())
 	b = a*5
@@ -7,59 +6,30 @@ def main():
 	for i in range(0, b):
 		c = input()
 		queslist.append(c)
-	# print(queslist)
-	newqueslist = []
-	i = 0
-	while i<len(queslist):
-		newlist1 = []
-		for j in range(i, i+5):
-			newlist1.append(queslist[j])
-		newqueslist.append(newlist1)
-		i = i + 5
-	# print(newqueslist)
-	queslist1 = newqueslist
-	i = 0
-	d = 1
-	while i<len(newqueslist):
-		quesdict[d] = newqueslist[i]
-		d = d + 1
-		i = i + 1
-	# print(quesdict)
-	adict = {}
-	noofpart = int(input())
-	# print(noofpart)
+	noofpart= int(input())
 	totpart = noofpart*(a+1)
-	# print(totpart)
 	i = 0
 	partlist = []
 	while i<totpart:
 		inp = input()
 		partlist.append(inp)
 		i = i + 1
-	# print(partlist)
 	major = Counter(partlist)
-	# print(major)
 	adict1 = {}
 	adict2 = {}
 	adict3 = {}
 	adict4 = {}
-	for key, value in major.items():
+	for key,value in major.items():
 		p = key.split(" ")
-		# print(p)
 		if len(p) == 2:
 			if int(p[0]) == 1 :
 				adict1[p[1]] = value
 			elif int(p[0]) == 2 :
 				adict2[p[1]] = value
-			elif int(p[0]) == 3:
+			elif int(p[0]) == 3 :
 				adict3[p[1]] = value
 			else:
 				adict4[p[1]] = value
-
-	# print(adict1)
-	# print(adict2)
-	# print(adict3)
-	# print(adict4)
 	list1 = []
 	for key, value in adict1.items():
 		list1.append(value)
@@ -75,10 +45,10 @@ def main():
 				print("Highest number of votes for question : Who will be the next cm for Telangana? : "+each)
 	list3 = []
 	if len(adict3) != 0:
-		for key, value in adict3.items():
-			list3.append(value)
-		for each in adict3:
-			if adict3[each] == max(list3):
+		for key, value in adict2.items():
+			list2.append(value)
+		for each in adict2:
+			if adict2[each] == max(list2):
 				print("Highest number of votes for question : Who will be the next cm for AP? : "+each)
 
 if __name__ == '__main__':
