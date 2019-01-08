5c6123c6c89613e2d5d977902b9b503394b57d80
heerak205 <heerak.praffull@msitprogram.net>
2019-01-08 20:31:12 +0530

    "cspp2_m10_a1_1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -2/10 "

--- cspp1-remidial/Poll/poll1.py
+++ cspp1-remidial/Poll/poll1.py
@@ -1,3 +1,4 @@
+from collections import Counter
 def main():
 	a = int(input())
 	b = a*5
@@ -32,28 +33,53 @@ def main():
 	i = 0
 	partlist = []
 	while i<totpart:
-		inp = input().split(" ")
-		if len(inp) == 2:
-			partlist.append(inp)
+		inp = input()
+		partlist.append(inp)
 		i = i + 1
 	# print(partlist)
-	for each in partlist:
-		if int(each[0]) not in adict:
-			adict[int(each[0])] = each[1]
-		else:
-			if adict.get(int(each[0])) == each[1]:
-				adict[int(each[0])] = each[1]
-	# print(adict)
-	# for each in adict:
-	# 	print(("Highest number of votes for question : Who should be the next Prime Minister? : ")+ adict[each])
-	for each in adict:
-		if each == 1:
-			print(("Highest number of votes for question : Who should be the next Prime Minister? : ")+ adict[each])
-		if each == 2:
-			print(("Highest number of votes for question : Who will be the next cm for Telangana? : ")+ adict[each])
-		if each == 3:
-			print(("Highest number of votes for question : Who will be the next cm for AP? : ")+ adict[each])
+	major = Counter(partlist)
+	# print(major)
+	adict1 = {}
+	adict2 = {}
+	adict3 = {}
+	adict4 = {}
+	for key, value in major.items():
+		p = key.split(" ")
+		# print(p)
+		if len(p) == 2:
+			if int(p[0]) == 1 :
+				adict1[p[1]] = value
+			elif int(p[0]) == 2 :
+				adict2[p[1]] = value
+			elif int(p[0]) == 3:
+				adict3[p[1]] = value
+			else:
+				adict4[p[1]] = value
 
+	# print(adict1)
+	# print(adict2)
+	# print(adict3)
+	# print(adict4)
+	list1 = []
+	for key, value in adict1.items():
+		list1.append(value)
+	for each in adict1:
+		if adict1[each] == max(list1):
+			print("Highest number of votes for question : Who should be the next Prime Minister? : "+each)
+	list2 = []
+	if len(adict2) != 0:
+		for key, value in adict2.items():
+			list2.append(value)
+		for each in adict2:
+			if adict2[each] == max(list2):
+				print("Highest number of votes for question : Who will be the next cm for Telangana? : "+each)
+	list3 = []
+	if len(adict3) != 0:
+		for key, value in adict3.items():
+			list3.append(value)
+		for each in adict3:
+			if adict3[each] == max(list3):
+				print("Highest number of votes for question : Who will be the next cm for AP? : "+each)
 
 if __name__ == '__main__':
 	main()
